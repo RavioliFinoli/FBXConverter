dab9e462150fa1bd44f3485894cc69dd29c8e832
Elias Finoli <elle_finoli@hotmail.com>
2018-09-30 22:25:24 +0200

    Converter now uses Stefan-version methods
    Stefan OP

--- FBXConverterProject1/FBXConverterProject/main.cpp
+++ FBXConverterProject1/FBXConverterProject/main.cpp
@@ -6,10 +6,13 @@ int main()
 #if defined(DEBUG) | defined(_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	Converter converter("test.fbx");
-	converter.convertFileToCustomFormat();
-	std::cout <<        "DONE"<< std::endl;
+	converter.convertFileToCustomFormatStefan();
+
+		SetConsoleTextAttribute(hConsole, 2);
+	std::cout << "Export finished."<< std::endl;
 
 
 	std::getchar();
