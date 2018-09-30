f307ccba475c49030fa1e5d575e0a0d49e0472a3
Elias Finoli <elle_finoli@hotmail.com>
2018-09-30 22:24:43 +0200

    Fixed and added more helper functions

--- FBXConverterProject1/FBXConverterProject/ExportHelpers.h
+++ FBXConverterProject1/FBXConverterProject/ExportHelpers.h
@@ -90,7 +90,7 @@ namespace FBXExport
 		}
 	};
 
-	void appendFloat4(std::ofstream& file, Vec4 vec)
+	void appendFloat4(std::ofstream& file, const Vec4 vec)
 	{
 		if (!file.is_open())
 			return;
@@ -98,12 +98,131 @@ namespace FBXExport
 		file.write((const char*)&vec, sizeof(Vec4));
 	}
 
-	void appendTransform(std::ofstream& file, DecomposedTransform transform)
+	/// Writes each Vec4 component of the Transform (order is T, R, S)
+	void appendTransform(std::ofstream& file, const DecomposedTransform transform)
 	{
 		if (!file.is_open())
 			return;
 
-		file.write((const char*)&transform, sizeof(Vec4));
+		/// Write each vector (T, R, S) (R is quaternion)
+		file.write((const char*)&transform.translation, sizeof(Vec4));
+		file.write((const char*)&transform.rotation, sizeof(Vec4));
+		file.write((const char*)&transform.scale, sizeof(Vec4));
 	}
+
+	void appendInt32(std::ofstream& file, const int32_t value)
+	{
+		if (!file.is_open())
+			return;
+
+		file.write((const char*)&value, sizeof(int32_t));
+	}
+
+	void appendBone(std::ofstream& file, const FBXExport::Bone& bone)
+	{
+		if (!file.is_open())
+			return;
+
+		/// Write each transform and parentIndex
+		appendTransform(file, bone.jointInverseBindPoseTransform);
+		appendTransform(file, bone.jointReferenceTransform);
+		appendInt32(file, bone.parentIndex);
+	}
+
+	/// Writes the bone count and each bone (order is inverseBindPose, referencePose, parentIndex)
+	void appendSkeleton(std::ofstream& file, const FBXExport::Skeleton& skeleton)
+	{
+		if (!file.is_open())
+			return;
+
+		/// Write bone count (needed for loading)
+		int32_t jointCount = skeleton.joints.size();
+		file.write((char*)&jointCount, sizeof(jointCount));
+
+		/// Write each joint
+		for (const auto& bone : skeleton.joints)
+		{
+			appendBone(file, bone);
+		}
+	}
+
+	///// Reads a Vec4 (four floats in x, y, z, w order)
+	//Vec4 loadVec4(std::ifstream& file)
+	//{
+	//	if (!file.is_open())
+	//		return FBXExport::Vec4();
+
+	//	Vec4 vec;
+	//	file.read((char*)&vec, sizeof(Vec4));
+
+	//	return vec;
+	//}
+
+	//int32_t loadInt32(std::ifstream& file)
+	//{
+	//	if (!file.is_open())
+	//		return 0;
+
+	//	int32_t int32;
+	//	file.read((char*)&int32, sizeof(int32_t));
+	//	return int32;
+	//}
+
+	//DecomposedTransform loadTransform(std::ifstream& file)
+	//{
+	//	if (!file.is_open())
+	//		return FBXExport::DecomposedTransform();
+
+	//	DecomposedTransform transform;
+
+	//	transform.translation = (loadVec4(file));
+	//	transform.rotation    = (loadVec4(file));
+	//	transform.scale       = (loadVec4(file));
+
+	//	return transform;
+	//}
+
+	//FBXExport::Bone loadBone(std::ifstream& file)
+	//{
+	//	if (!file.is_open())
+	//		return FBXExport::Bone();
+
+	//	FBXExport::Bone bone;
+	//	bone.jointInverseBindPoseTransform = loadTransform(file);
+	//	bone.jointReferenceTransform       = loadTransform(file);
+	//	bone.parentIndex                   = loadInt32(file);
+
+	//	return bone;
+	//}
+
+	//Skeleton loadSkeleton(std::ifstream& file, int32_t boneCount)
+	//{
+	//	if (!file.is_open())
+	//		return FBXExport::Skeleton();
+
+	//	/// Read and add each bone to vector
+	//	Skeleton skeleton;
+	//	for (int i = 0; i < boneCount; i++)
+	//		skeleton.joints.push_back(loadBone(file));
+	//	
+	//	return skeleton;
+	//}
+
+	//// If no boneCount is supplied, we assume we read that data first
+	//Skeleton loadSkeleton(std::ifstream& file)
+	//{
+	//	if (!file.is_open())
+	//		return FBXExport::Skeleton();
+
+	//	/// Read bone count
+	//	int32_t boneCount = loadInt32(file);
+
+	//	/// Read and add each bone to vector
+	//	Skeleton skeleton;
+	//	for (int i = 0; i < boneCount; i++)
+	//		skeleton.joints.push_back(loadBone(file));
+
+	//	return skeleton;
+	//}
 }
 
